d7928c879da562aec4258a403b067fe5b7ff0ef5
ocro <115986703+ocrovest@users.noreply.github.com>
2024-11-03 14:53:51 +0530

    Added error handling for custom models.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-
 @app.route('/favicon.ico')
 def favicon():
     return url_for('static', filename='images/open_moji_leg.png')
@@ -17,27 +16,29 @@ def rewrite():
     data = request.get_json()
     text = data.get("text")
     task = data.get("task")
-    
-    agent = Agent(task=task, content=text)
+    model = data.get("model")
+
+    agent = Agent(model=model, task=task, content=text)
 
     return jsonify(agent)
 
 @app.route("/summarize", methods=["POST"])
 def summarize():
+    print("Got Request")
     data = request.get_json()
     text = data.get("text")
+    model = data.get("model")
 
-    agent = Agent(task="summarize", content=text, rewrite=False)
-    time = agent["time"]
-    response = agent["response"]
-
-    print(time)
+    agent = Agent(model=model, task="summarize", content=text, rewrite=False)
 
     return jsonify(agent)
 
-@app.route("/random", methods=["GET"])
+@app.route("/random", methods=["POST"])
 def random_text():
-    agent = Agent(task="random", content="Write about anything", rewrite=False)
+    print("Got Request")
+    data = request.get_json()
+    model = data.get("model")
+    agent = Agent(model=model, task="random", content="Write about anything", rewrite=False)
     return jsonify(agent)
 
 if __name__ == "__main__":
